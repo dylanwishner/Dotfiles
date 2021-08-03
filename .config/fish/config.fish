328e790818f9d8654c9899607dda72b3ff5e951b
Dylan Wishner <dylanwishner@gmail.com>
2021-08-03 00:21:44 -0700

    Initial commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,39 @@
+#                   __ _          __ _     _     
+#   ___ ___  _ __  / _(_) __ _   / _(_)___| |__  
+#  / __/ _ \| '_ \| |_| |/ _` | | |_| / __| '_ \ 
+# | (_| (_) | | | |  _| | (_| |_|  _| \__ \ | | |
+#  \___\___/|_| |_|_| |_|\__, (_)_| |_|___/_| |_|
+#                        |___/                   
+
+function fish_greeting
+        echo (set_color blue; date +"%a %b %d, %r"; set_color normal);
+end;
+
+set_theme_gruvbox
+set theme_color_scheme gruvbox
+
+# bobthefish prompt customization
+set -g theme_display_date no
+set -g theme_powerline_fonts yes
+set -g theme_nerd_fonts yes
+
+# Add cargo bins to fish's path
+fish_add_path -g [~/.cargo/bin/]
+
+# Git abbreviations
+abbr -a -g g git
+abbr -a -g ga git add
+abbr -a -g gb git branch
+abbr -a -g gc git commit
+abbr -a -g gch git checkout
+abbr -a -g gl git log
+abbr -a -g gp git push
+abbr -a -g gs git status
+
+# Cargo abbreviations
+abbr -a -g ca cargo
+abbr -a -g cb cargo build
+abbr -a -g cad cargo doc
+abbr -a -g cado cargo doc --open
+abbr -a -g cr cargo run
+abbr -a -g ct cargo test 
