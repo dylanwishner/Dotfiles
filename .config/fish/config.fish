87c9d4d8afaf2b45a211a4d2063e38d3cbf5217a
Dylan Wishner <dylanwishner@gmail.com>
2021-08-24 16:37:10 -0700

    Added fixed fish configs

---
+++ .config/fish/config.fish
@@ -0,0 +1,30 @@
+#                   __ _          __ _     _     
+#   ___ ___  _ __  / _(_) __ _   / _(_)___| |__  
+#  / __/ _ \| '_ \| |_| |/ _` | | |_| / __| '_ \ 
+# | (_| (_) | | | |  _| | (_| |_|  _| \__ \ | | |
+#  \___\___/|_| |_|_| |_|\__, (_)_| |_|___/_| |_|
+#                        |___/                   
+
+function fish_greeting
+    echo (set_color blue; date +"%a %b %d, %r"; set_color normal)
+end
+
+starship init fish | source
+
+set_theme_gruvbox
+
+# Add cargo bins to fish's path
+fish_add_path -g [~/.cargo/bin/]
+
+# Cargo abbreviations
+abbr -a -g ca cargo
+abbr -a -g cb cargo build
+abbr -a -g cad cargo doc
+abbr -a -g cado cargo doc --open
+abbr -a -g cr cargo run
+abbr -a -g ct cargo test
+
+# Misc abbreviations
+abbr -a -g ls exa
+abbr -a -g envim nvim ~/.config/nvim/init.lua
+abbr -a -g vim nvim
